e9f9f69214d223cc06a34844dfead2cb70f11676
Thilde Bisgaard Jakobsen <eaa25tbja@students.eaaa.dk>
2025-10-31 08:28:48 +0100

    1st commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const quotesListe = [
+  {
+    author: "Eminem",
+    quote: "I'm stupid, I'm ugly, I'm dumb, I smell. Did I mention I'm stupid?",
+  },
+  {
+    author: "Betty White",
+    quote:
+      "My mother always used to say: The older you get, the better you get, unless you’re a banana.",
+  },
+  {
+    author: "Mark Twain",
+    quote:
+      "Clothes make the man. Naked people have little or no influence in society.",
+  },
+  {
+    author: "Rita Rudner",
+    quote:
+      "I love being married. It’s so great to find that one special person you want to annoy for the rest of your life.",
+  },
+  {
+    author: "Phyllis Diller",
+    quote:
+      "I want my children to have all the things I couldn’t afford. Then I want to move in with them.",
+  },
+];
